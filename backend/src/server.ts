b3d797fda83c1e420bcb49e68c8a91b0e7560f5d
Adityamkumar <adityasharma841460@gmail.com>
2026-08-18 15:48:18 +0530

    fix: remove unused dns import and set default result order in server configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,7 @@
 import "dotenv/config.js";
-import dns from "node:dns";
 import app from "./app.js";
 import connectDB from "./db/db.js";
 import logger from "./lib/logger.js";
-
-dns.setDefaultResultOrder('ipv4first')
 
 const PORT = process.env.PORT || 8000
 
