4bd76856082cebbf38cfb6a1d390e85dd6776f90
Adityamkumar <adityasharma841460@gmail.com>
2026-08-18 17:14:28 +0530

    feat: replace nodemailer with Brevo for email sending functionality

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -1,5 +1,4 @@
-import nodemailer from "nodemailer";
-
+import { BrevoClient } from "@getbrevo/brevo";
 
 interface EmailOptions {
   to: string;
@@ -7,18 +6,29 @@ interface EmailOptions {
   html: string;
 }
 
-export const sendEmail = async ({ to, subject, html }: EmailOptions) => {
-  const transporter = nodemailer.createTransport ({
-    service:'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-  await transporter.sendMail({
-    from: `"Lumora" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+const brevo = new BrevoClient({
+  apiKey: process.env.BREVO_API_KEY!,
+});
+
+export const sendEmail = async ({ to, subject, html }: EmailOptions):Promise<void> => {
+  try {
+    const result = await brevo.transactionalEmails.sendTransacEmail({
+      sender: {
+        name: process.env.BREVO_SENDER_NAME || "Lumora",
+        email: process.env.BREVO_SENDER_EMAIL!,
+      },
+      to: [
+        {
+          email: to,
+        },
+      ],
+      subject,
+      htmlContent: html,
+    });
+
+    console.log("Brevo email sent:", result.messageId);
+  } catch (error) {
+    console.error("Brevo email error:", error);
+    throw new Error("Failed to send email");
+  }
 };
